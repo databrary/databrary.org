ef4868e756e9cd06beccecf4cc9d4ea5762cba60
Jeffrey Spies <code@jeffspies.com>
2020-02-14 01:14:30 -0500

    Fixed the apollo client to dynamically link auth

--- client/src/boot/apollo.js
+++ client/src/boot/apollo.js
@@ -3,23 +3,29 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 import VueApollo from 'vue-apollo'
 import fetch from 'node-fetch'
 import { createHttpLink } from 'apollo-link-http'
+import { setContext } from 'apollo-link-context'
 // import { Cookies } from 'quasar'
 
 // console.log(Cookies.getAll())
 
 export default async ({ app, store, Vue }) => {
-  console.log('apollo sessionid', store.getters['app/sessionId'])
   const httpLink = createHttpLink({
     uri: 'http://localhost:8002/v1/graphql',
-    fetch,
-    headers: {
-      sessionID: store.getters['app/sessionId']
+    fetch
+  })
+
+  // We're using apollo-link-context to dynamically set the header
+  const authLink = setContext((_) => {
+    return {
+      headers: {
+        sessionID: store.getters['app/sessionId']
+      }
     }
   })
 
   // Create the apollo client
   const apolloClient = new ApolloClient({
-    link: httpLink,
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
     connectToDevTools: true
   })
