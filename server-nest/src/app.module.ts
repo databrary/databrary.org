11a9e9dceaa1ebe4b828ef4002e01b60d364e82b
Reda Nezzar <nezzar.reda@gmail.com>
2020-05-12 12:56:20 -0400

    Removed env folder and consolidated all env var in one foledr

--- server-nest/src/app.module.ts
+++ server-nest/src/app.module.ts
@@ -2,15 +2,16 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { KeycloakModule } from './keycloak/keycloak.module'
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    envFilePath: process.env.NODE_ENV === 'development' 
-      ? ['./env/dev.env','../.env'] 
-      : ['./env/prod.env','../.env'],
-    isGlobal: true,
-    expandVariables: true,
-  })],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['../.env'],
+      isGlobal: true,
+    }),
+    KeycloakModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
