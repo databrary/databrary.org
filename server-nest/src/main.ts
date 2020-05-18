57d9b838fb4d43c7f1d1ada7bd5d9dd8da2c520b
Jeffrey Spies <code@jeffspies.com>
2020-05-18 15:19:34 -0400

    Caddy will not work for Linux

--- server-nest/src/main.ts
+++ server-nest/src/main.ts
@@ -2,8 +2,6 @@ import { NestFactory } from '@nestjs/core'
 import { ConfigService } from '@nestjs/config'
 import { AppModule } from './app.module'
 
-const proxy = require('express-http-proxy')
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const configService = app.get(ConfigService)
@@ -12,8 +10,6 @@ async function bootstrap() {
   const port = configService.get('SERVER_PORT')
   const proxyUrl = configService.get('APP_URL_PROXY')
 
-  // proxy order is messed up
-  // app.use('/', proxy('http://localhost:8080'))
   await app.listen(port)
 }
 bootstrap()
