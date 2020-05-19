50260fa95ff83707d5069664286d1f838ec92f24
Reda Nezzar <nezzar.reda@gmail.com>
2020-05-19 17:16:28 -0400

    yarn lock fine and code cleaning

--- server-nest/src/main.ts
+++ server-nest/src/main.ts
@@ -2,18 +2,10 @@ import { NestFactory } from '@nestjs/core'
 import { ConfigService } from '@nestjs/config'
 import { AppModule } from './app.module'
 
-const proxy = require('express-http-proxy')
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const configService = app.get(ConfigService)
-
-  // Get env variables
   const port = configService.get('SERVER_PORT')
-  const proxyUrl = configService.get('APP_URL_PROXY')
-
-  // proxy order is messed up
-  // app.use('/', proxy('http://localhost:8080'))
   await app.listen(port)
 }
 bootstrap()
