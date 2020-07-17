afb208c3697fdfe351382d7bffde4c1c7d3e5e3b
TheoWolf <nezzar.reda@gmail.com>
2020-07-17 17:09:48 -0400

    Resolve Merge conflict

--- server-nest/test/recorderExample.ts
+++ server-nest/test/recorderExample.ts
@@ -1,33 +1,20 @@
-import { Polly } from '@pollyjs/core';
-import FSPersister from '@pollyjs/persister-fs';
+import { Polly } from '@pollyjs/core'
+import FSPersister from '@pollyjs/persister-fs'
 import NodeHttpAdapter from '@pollyjs/adapter-node-http'
-import gql from 'graphql-tag';
+// import gql from 'graphql-tag'
 
-async function main () {
-  Polly.register(NodeHttpAdapter);
-  Polly.register(FSPersister);
+// async function main () {
 
-  const polly = new Polly('Recording some feature', {
-    adapters: ['node-http'],
-    persister: 'fs',
-    persisterOptions: {
-      fs: {
-        recordingsDir: './recordings' // TODO figure out where this goes
-      }
+Polly.register(NodeHttpAdapter)
+Polly.register(FSPersister)
+
+// export const Polly = P
+export const polly = new Polly('Recording some feature', {
+  adapters: ['node-http'],
+  persister: 'fs',
+  persisterOptions: {
+    fs: {
+      recordingsDir: './recordings' // TODO figure out where this goes
     }
-  });
-  
-  const result = await client.query({ // TODO get graphql client from somewhere
-    query: gql`
-      query MyQuery {
-        assets {
-          id
-          name
-        }
-      }
-    `
-  })
-  await polly.stop()
-}
-
-main()
+  }
+})
