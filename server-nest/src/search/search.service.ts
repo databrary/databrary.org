4f4b8e425b22ff0b236fc4efed0a5a3c99136c5d
TheoWolf <nezzar.reda@gmail.com>
2020-07-28 18:36:14 -0400

    Retry on conflict during elastic search updates

--- server-nest/src/search/search.service.ts
+++ server-nest/src/search/search.service.ts
@@ -29,6 +29,7 @@ export class SearchService {
         id: id,
         index: index,
         refresh: refresh ? 'true' : 'false',
+        retry_on_conflict: 3,
         body: { doc: doc }
       })
       return statusCode
