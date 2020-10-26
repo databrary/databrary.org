4ecc970278675c4ace0e3cc4d2f99c8d44f64efd
TheoWolf <nezzar.reda@gmail.com>
2020-10-26 16:08:39 -0400

    Add New plugins to Uppy uploader

--- client/src/components/upload/FileUploader.vue
+++ client/src/components/upload/FileUploader.vue
@@ -8,6 +8,14 @@
 import Uppy from '@uppy/core'
 import Dashboard from '@uppy/dashboard'
 import AwsS3 from '@uppy/aws-s3'
+import GoogleDrive from '@uppy/google-drive'
+import Dropbox from '@uppy/dropbox'
+import Instagram from '@uppy/instagram'
+import Facebook from '@uppy/facebook'
+import OneDrive from '@uppy/onedrive'
+import Webcam from '@uppy/webcam'
+import ScreenCapture from '@uppy/screen-capture'
+import ImageEditor from '@uppy/image-editor'
 
 import { mapActions } from 'vuex'
 
@@ -41,47 +49,57 @@ export default {
     try {
       this.uppy = Uppy({
         id: 'FilesUploader'
-      }).use(Dashboard, {
-        inline: true,
-        target: '#files-upload-area'
-      }).use(AwsS3, {
-        async getUploadParameters (file) {
-          return fetch('/minio/sign-upload', {
-            method: 'post',
-            headers: {
-              accept: 'application/json',
-              'content-type': 'application/json'
-            },
-            body: JSON.stringify({
-              filename: file.name,
-              contentType: file.type,
-              format: file.extension,
-              assetId: await that.insertAsset(
-                {
-                  name: file.name,
-                  assetType: 'file',
-                  privacyType: 'private',
-                  parentId: that.parentId
-                }
-              ),
-              uploadType: 'file'
-            })
-          }).then((response) => {
-            console.log('response', response)
-            return response.json()
-          }).then((data) => {
-            console.log('data', data)
-            return {
-              method: data.method,
-              url: data.url,
-              fields: data.fields,
-              headers: data.headers
-            }
-          }).catch((error) => {
-            console.log(`Uppy error ${error}`)
-          })
-        }
       })
+        .use(Dashboard, {
+          inline: true,
+          target: '#files-upload-area'
+        })
+        .use(GoogleDrive, { inline: true, target: Dashboard, companionUrl: 'https://companion.uppy.io'
+        }).use(Dropbox, { inline: true, target: Dashboard, companionUrl: 'https://companion.uppy.io'
+        }).use(Instagram, { inline: true, target: Dashboard, companionUrl: 'https://companion.uppy.io'
+        }).use(Facebook, { inline: true, target: Dashboard, companionUrl: 'https://companion.uppy.io'
+        }).use(OneDrive, { inline: true, target: Dashboard, companionUrl: 'https://companion.uppy.io'
+        }).use(Webcam, { inline: true, target: Dashboard, companionUrl: 'https://companion.uppy.io'
+        }).use(ScreenCapture, { inline: true, target: Dashboard, companionUrl: 'https://companion.uppy.io'
+        })
+        .use(AwsS3, {
+          async getUploadParameters (file) {
+            return fetch('/minio/sign-upload', {
+              method: 'post',
+              headers: {
+                accept: 'application/json',
+                'content-type': 'application/json'
+              },
+              body: JSON.stringify({
+                filename: file.name,
+                contentType: file.type,
+                format: file.extension,
+                assetId: await that.insertAsset(
+                  {
+                    name: file.name,
+                    assetType: 'file',
+                    privacyType: 'private',
+                    parentId: that.parentId
+                  }
+                ),
+                uploadType: 'file'
+              })
+            }).then((response) => {
+              console.log('response', response)
+              return response.json()
+            }).then((data) => {
+              console.log('data', data)
+              return {
+                method: data.method,
+                url: data.url,
+                fields: data.fields,
+                headers: data.headers
+              }
+            }).catch((error) => {
+              console.log(`Uppy error ${error}`)
+            })
+          }
+        })
     } catch (error) {
 
     }
