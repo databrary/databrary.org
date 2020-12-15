c967588979bbe823037cc3bda77f41b2a721ac78
TheoWolf <nezzar.reda@gmail.com>
2020-12-15 10:11:10 -0500

    Return void on hasura auth webhook

--- server-nest/src/hasura/hasura.controller.ts
+++ server-nest/src/hasura/hasura.controller.ts
@@ -1,10 +1,10 @@
-import { Controller, Session, Request, Res, Get, Post, Body } from '@nestjs/common'
+import { Controller, Session, Res, Get } from '@nestjs/common'
 
 @Controller('hasura')
 export class HasuraController {
   @Get('auth')
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  async hasura (@Session() { user }, @Res() res): Promise<unknown> {
+  async hasura (@Session() { user }, @Res() res): Promise<void> {
     if (user == null) return res.status(401).send()
 
     res.json({
